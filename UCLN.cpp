0df4157c63244b8015a72d34fc457ac2fb6b934a
kynguyen321 <72696885+kynguyen321@users.noreply.github.com>
2023-03-30 17:31:18 +0700

    Create UCLN.cpp

---
+++ UCLN.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    ll a, b; cin >> a >> b;
+    cout << __gcd(a, b);
+
+    return 0;
+}
